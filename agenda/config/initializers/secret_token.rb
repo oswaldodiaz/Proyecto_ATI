d330e242617363fcd0fb00b646f08208d7042184
Oswaldo Diaz <oswaldoandresdiaz@gmail.com>
2012-02-23 01:15:06 -0430

    primera insercion

---
+++ agenda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agenda::Application.config.secret_token = '8774973c7eb8ee2d864d4952ffad59583b140dceb6d7369337efd5f220e15e26a45022da667168bb8b8b166e263fa715c9d48b2eb5cbf0d0d420d78117680326'
---
+++ cliente/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cliente::Application.config.secret_token = '3c45d1e6e7cf23fafa0e5f7f6bf0c9c92c150a4526c0652dbfc4b76df4bfcf379fbd6e11eb62a8b692f9acca9eca64f665c4b60b24faa5094e03bf2b51b49bf2'
